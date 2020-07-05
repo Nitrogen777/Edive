bbec8e184f698614815361c54b86dbfd3ab674d4
EyShap <eyalm1000@gmail.com>
2020-07-05 22:18:09 +0300

    -Added message sending capabilities to site

--- site/edive/src/index.js
+++ site/edive/src/index.js
@@ -1,34 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-class Api extends React.Component{
-    constructor(props){
-        super(props);
 
-        this.state = {
-            data: null
-        };
+
+async function sendMessageToServer(serverId, msg){
+    try{
+        let body = {serverId: serverId, msg: msg}
+        fetch("/api/test", {
+            method: 'post',
+            body: JSON.stringify(body),
+            headers: { 'Content-Type': 'application/json' },
+        })
+    } catch (err) {
+        console.log(err);
     }
-    componentDidMount() {
-        this.getApiData()
+}
+
+
+class SendMessageForm extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {serverId: "", msg: ""}
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
-    getApiData = async() => {
-        try{
-            let text = await fetch("/api/test").then(res => res.text())
-            this.setState({ data: text})
-        } catch (err) {
-            console.log(err);
+
+    handleSubmit(){
+        if(this.state.serverId === ""){
+            alert("serverId is empty")
+        }
+        if(this.state.msg.length > 100){
+            alert("Character limit for message is 100 characters")
+        }
+        sendMessageToServer(this.state.serverId, this.state.msg)
+    }
+    handleChange(event){
+        if(event.target.name === "msg"){
+            this.setState({serverId: this.state.serverId, msg: event.target.value})
+        }else if(event.target.name === "serverId"){
+            this.setState({serverId: event.target.value, msg: this.state.msg})
         }
     }
     render(){
         return(
-        <h1>{this.state.data}</h1>
+            <form onSubmit = {this.handleSubmit}>
+                <label>Server Id</label>
+                <input name = "serverId" type="text" value={this.state.serverId} onChange={this.handleChange} />
+                <label>Message </label>
+                <input name = "msg" type="text" value={this.state.msg} onChange={this.handleChange} />
+                <input type="submit" value="Submit" />
+            </form>
         )
     }
 }
 ReactDOM.render(
-    <div>
-        <h1>Hello, world!</h1>
-        <Api />
-    </div>,
+    <SendMessageForm/>,
     document.getElementById('root')
 );
